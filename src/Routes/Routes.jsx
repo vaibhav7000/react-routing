23a1acef9eab20e735e991a63b955312cfaf1cd1
vaibhav chawla <vaibhav.chawla@roundglass.com>
2025-06-25 22:47:39 +0530

    more routing code

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Outlet, useParams } from "react-router";
 import { Home } from "../Components/Home";
 import { About } from "../Components/About";
 import { MarketingLayout, MarketingRobouts, MarketingGagets } from "../Components/MarketingLayout";
@@ -29,9 +29,52 @@ export default function AppRoutes() {
                 </Route>
             </Route>
 
+            {/* Defining nested routes type 3 - parent with both path and element */}
+            <Route path="food" element={<RootFoodWrapper/>}>
+                <Route index element={<FoodCart/>} />
+
+                <Route path=":foodItem" element={<SingleFood/>} />
+
+            </Route>
+
         </Routes>
     )
 }
 
 // we can declare a single route with no path but it does not contains any nesting routes
 // when declaring nested-routes either parent has the path -> type 2 ( in this case it should has default child as element ), eor it has element that will predent as the Layout
+
+
+
+function RootFoodWrapper() {
+
+    return (
+        <div style={{
+            padding: 40, backgroundColor: "", boxShadow: "0px 0px 5px 10px rgb(247, 247, 247)"
+        }}>
+            <div>
+                FoodWrapper
+            </div>
+            <Outlet/>
+        </div>
+    )
+}
+
+function FoodCart() {
+
+    return (
+        <>
+            Food-Cart
+        </>
+    )
+}
+
+function SingleFood() {
+    const { foodItem } = useParams();
+
+    return (
+        <>
+            FoodItem is : {foodItem}
+        </>
+    )
+}
